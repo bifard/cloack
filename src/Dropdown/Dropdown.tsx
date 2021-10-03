60d34d7b9339830d522f501312b42f6d3241cedd
Alexey <Leks11172@mail.ru>
2021-10-03 23:09:12 +0700

    nativ select

--- src/Dropdown/Dropdown.tsx
+++ src/Dropdown/Dropdown.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { ChangeEvent } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { updateListClock } from "../store/listClock/listClockAction";
 import { IItemListClock } from "../store/listClock/listClockReducer";
@@ -17,31 +17,23 @@ export const Dropdown = ({ list, name, id, isOpen }: IPropsDropdown) => {
   const listClock = useSelector<RootState, Array<IItemListClock>>(state => state.listClock);
   const timeZone = useSelector<RootState, Array<ITimezone>>(state => state.timezone.data);
   const dispatch = useDispatch();
-  function onChange(e: React.MouseEvent<HTMLButtonElement | HTMLLIElement>) {
-    const zone = timeZone.filter(item => item.name === e.currentTarget.textContent)[0];
+  function onChange(e: ChangeEvent<HTMLSelectElement>) {
+    const zone = timeZone.filter(item => item.name === e.target.value)[0];
     dispatch(updateListClock(listClock.map(item => item.id === id ? { name: e.currentTarget.textContent, id: id, timeZone: Number(zone.timezone), isOpen: !isOpen } : item)))
   }
   return (
     <div className={style.container}>
-      <button
-        className={style.button}
-        onClick={e => onChange(e)} id={id}>
-        {name}</button>
-
-      {
-        isOpen &&
-        <ul className={style.list}>
-          {list.map((item, index) => (
-            <li
-              key={index}
-              onClick={e => onChange(e)}
-              id={item.name + id}
-            >
-              {item.name}</li>
-          ))}
-        </ul>
-
-      }
+      <select className={style.select} onChange={e => onChange(e)}>
+        {list.map((item, index) => (
+          <option 
+            className ={style.option}
+            value={item.name}
+            key={index}
+            id={item.name + id}
+          >
+            {item.name}</option>
+        ))}
+      </select>
     </div>
   );
 }
